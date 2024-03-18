4a12a5342ba888bc545e196f26f2008f6e010b38
AnttoLion <anttolion@gmail.com>
2024-03-18 09:45:40 -0400

    A minor change for test token

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const App = () => {
   useEffect(() => {
     getTestToken((jsonRes:any)=>{
       setAccessToken(jsonRes.refreshToken);
+      localStorage.setItem('access-token', jsonRes.refreshToken);
     });
   }, []);
 
