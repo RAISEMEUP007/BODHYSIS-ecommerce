ca5e7d8f5e94e734a41c13f06d5e2fb6afdfd8ad
AnttoLion <anttolion@gmail.com>
2024-04-29 16:21:56 -0400

    Default email & phone numbers

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -191,6 +191,8 @@ const Login: React.FC = () => {
           localStorage.setItem('access-token', jsonRes.refreshToken);
           localStorage.setItem('full-name', jsonRes.fullName);
           localStorage.setItem('customerId', jsonRes.customerId);
+          localStorage.setItem('customer_email', jsonRes.email);
+          localStorage.setItem('customer_phone_number', jsonRes.phone_number);
           navigate('/reservation');
           break;
         case 403:
